d43916dd1c8df0e4dfc25dfa85385dce7c41df59
Krzysztof Mirończuk <krzysztof.mironczuk@socart.media>
2022-11-05 16:44:46 +0100

    double render problem

---
+++ server.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const socket = require('socket.io');
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+
+const tasks = [];
+
+const server = app.listen(8000, () => {
+    console.log('Server running on port: 8000');
+})
+
+app.use((req, res) => {
+    res.status(404).send({ message: 'Not found...' });
+  });
+
+const io =socket(server);
+
+io.on('connection', (socket) => {
+    console.log(socket.id)
+    io.to(socket.id).emit('updateData', tasks); 
+    console.log('actual tasks', tasks)
+
+  
+    socket.on('addTask', (task) => {
+        tasks.push(task);
+        socket.broadcast.emit('addTask', task);
+        console.log('addTask')
+        console.log('actual tasks', tasks)
+
+    });
+
+    socket.on('removeTask', (taskId) => {
+        tasks.splice((tasks.indexOf(tasks.find(task => taskId == task.id))), 1)
+        socket.broadcast.emit('removeTask', taskId);
+        console.log('removeTask')
+        console.log('actual tasks', tasks)
+
+    });
+
+    socket.on('editTask', (payload) => {
+       const changedTask = {name: payload.name};
+       console.log(changedTask)
+        tasks.map(task => (task.id === payload.id ? {...task, ...changedTask } : task))
+        socket.broadcast.emit('editTask', payload)
+        console.log('editTask')
+        console.log('actual tasks', tasks)
+    })
+  });
+
